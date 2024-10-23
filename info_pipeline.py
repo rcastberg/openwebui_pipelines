d8bf2c2de03d43019127a5a2ef86ae175303cf4b
Rene Castberg <Rene.Castberg@basefarm-orange.com>
2024-10-23 08:08:11 +0200

    New test

--- info_pipeline.py
+++ info_pipeline.py
@@ -4,9 +4,27 @@ import subprocess
 
 
 class Pipeline:
+    class Valves(BaseModel):
+        # List target pipeline ids (models) that this filter will be connected to.
+        # If you want to connect this filter to all pipelines, you can set pipelines to ["*"]
+        # e.g. ["llama3:latest", "gpt-3.5-turbo"]
+        pipelines: List[str] = []
+
+        # Assign a priority level to the filter pipeline.
+        # The priority level determines the order in which the filter pipelines are executed.
+        # The lower the number, the higher the priority.
+        priority: int = 0
+
     def __init__(self):
+        self.type = "filter"
         self.id = "info_pipeline"
         self.name = "Info Pipeline"
+        self.valves = self.Valves(
+            **{
+                "pipelines": ["*"],  # Connect to all pipelines
+            }
+        )
+        self.model = None
         pass
 
     async def on_startup(self):
@@ -19,30 +37,20 @@ class Pipeline:
         print(f"on_shutdown:{__name__}")
         pass
 
-    def execute_python_code(self, code):
-        try:
-            #result = subprocess.run(
-            #    ["python", "-c", code], capture_output=True, text=True, check=True
-            #)
-            #stdout = result.stdout.strip()
-            #return stdout, result.returncode
-            return "Ttest", 200
-        except subprocess.CalledProcessError as e:
-            return e.output.strip(), e.returncode
-
-    def pipe(
-        self, user_message: str, model_id: str, messages: List[dict], body: dict
-    ) -> Union[str, Generator, Iterator]:
-        # This is where you can add your custom pipelines like RAG.
-        print(f"pipe:{__name__}")
+    async def inlet(self, body: dict, user: Optional[dict] = None) -> dict:
+        # This filter is applied to the form data before it is sent to the OpenAI API.
+        print(f"inlet:{__name__}")
 
         print(messages)
         print(user_message)
         print(body)
+        user_message = body["messages"][-1]["content"]
 
-        if body.get("title", False):
-            print("Title Generation")
-            return "Python Code Pipeline"
-        else:
-            stdout, return_code = self.execute_python_code(user_message)
-            return stdout
+        # Filter out toxic messages
+        toxicity = self.model.predict(user_message)
+        print(toxicity)
+
+        if toxicity["toxicity"] > 0.5:
+            raise Exception("Toxic message detected")
+
+        return body
